b8aaca9fa165ea4b4da540d948ce6db33b6f07dd
Tyler Brittain <tbrittain100@gmail.com>
2024-01-19 23:57:59 -0500

    implement crawlPage w/ tests

--- crawl.js
+++ crawl.js
@@ -24,7 +24,29 @@ function getURLsFromHTML(htmlBody, baseURL) {
   });
   return urls;
 }
+
+async function crawlPage(baseUrl) {
+  const resp = await fetch(baseUrl, {
+    method: "GET",
+    "content-type": "text/html",
+  });
+  try {
+    // console.log(resp);
+    if (!resp.headers.get("content-type").includes("text/html")) {
+      throw new Error("content-type is not text/html");
+    }
+    if (resp.status >= 400) {
+      throw new Error(`status code is ${resp.status}`);
+    }
+    return await resp.text();
+  } catch (error) {
+    console.log(`error:${error}`);
+    return `error:${error}`;
+  }
+}
+
 module.exports = {
   normalizeUrl,
   getURLsFromHTML,
+  crawlPage,
 };
--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-// const { normalizeUrl } = require("./crawl.js");
-console.log(normalizeUrl("https://www.google.com"));
+const { crawlPage } = require("./crawl");
+
+async function main() {
+  console.log("lmao");
+  console.log(process.argv);
+  if (process.argv.length !== 3) {
+    console.log("error, improper number of args");
+  } else {
+    const baseUrl = process.argv[2];
+    console.log(await crawlPage(baseUrl));
+  }
+}
+
+main();
